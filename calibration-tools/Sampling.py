a62f90c83ae2a38cfccfb491ccd89aa4805115a9
Anja <anja.janischewski@mailbox.org>
2023-07-01 14:21:41 -0600

    Sampling method now uses the scipy.stats module, nearly orthogonal sampling replaced by orthogonal sampling

--- calibration-tools/Sampling.py
+++ calibration-tools/Sampling.py
@@ -1,73 +1,49 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Sat Jun 24 19:29:50 2023
-
-@author: Anja Janischewski
-
-Code is partly taken from the lecture material of the course "Computational Economics II"
-at Chemnitz University of Technology tought by Prof. Torsten Heinrich
+Project "abm-from-data" at the Complex Systems Summer School 2023 (Santa Fe Institute)
 """
 
-""" module imports """
 import numpy as np
-import pandas as pd
+from scipy.stats import qmc
+import matplotlib.pyplot as plt
 
 """ class definitions """
+
 class Sampling():
     """
-    class defining different methods for sampling the parameter space
+    collection of different methods of parameter space sampling 
     """
     def __init__(self, param_names, min_values, max_values):
         self.param_names = param_names
         self.min_values = min_values
         self.max_values = max_values
         self.nr_params = len(param_names)
-        self.parameter_samples = {}
     
-    def NOHL_sampling(self, nr_values):
+    def latin_hypercubes(self, nr_values, orthogonal=False):
         """
-        computes samples according to an nearly orthogonal hypercubes 
-        algorithm
+        computes evenly spaced samples from a parameter space, 
+        using the orthogonal latin hypercubes sampling algorithm from scipy
         
         Parameters
         ----------
         nr_values : int
             number of sampling values along each parameter dimension
             
+        orthogonal : bool
+            option to use orthogonal sampling. In this case, the number of
+            sampling values needs to be equal to the square of a prime number
+            
         Returns
         -------
-        samples_df : pandas data frame
-            data frame containing the sample points
+        samples : numpy array
+            two dimensional numpy array containing the sample points
         """
-        """ prepare parameter values for sampling """
-        parameters_for_sampling = {}
-        for k in range(self.nr_params):
-            min_value = self.min_values[k]
-            max_value = self.max_values[k]
-            step_size = (max_value - min_value) / (nr_values -1)
-            param_values = np.arange(min_value, max_value + step_size, step_size)
-            parameters_for_sampling[self.param_names[k]] = param_values
-            
-        """ create nearly orthogonal samples """
-        samples_ok = False
-        while not samples_ok:
-            """ create candidate samples"""
-            for param in self.param_names:
-                sample = np.random.choice(a=parameters_for_sampling[param],
-                                              size=nr_values, 
-                                              replace=False)
-                self.parameter_samples[param] = sample
-
-            """ check if samples are correlated"""
-            samples_df = pd.DataFrame(self.parameter_samples)
-            correlation_matrix = samples_df.corr() - \
-                                np.identity(self.nr_params)
-            if (np.abs(correlation_matrix) < 0.05).all(axis=None):
-                """ When samples are not correlated any more, we can
-                    stop the while loop."""
-                samples_ok = True
-        return samples_df
+        if orthogonal:
+            sampler = qmc.LatinHypercube(d=self.nr_params, strength=2)
+        else:
+            sampler = qmc.LatinHypercube(d=self.nr_params)
+        sample_unit_cube = sampler.random(n=nr_values)
+        sample = qmc.scale(sample_unit_cube, self.min_values, self.max_values)
+        return sample
     
     def grid_sampling(self, nr_values):
         """
@@ -88,8 +64,20 @@ class Sampling():
     
 """ main entry point """
 if __name__ == "__main__":
-    S = Sampling(param_names=["A", "B"],
-                 min_values=[0,0],
-                 max_values=[1,1])
-    sampling_df = S.NOHL_sampling(nr_values=11)
-    print(sampling_df)
+    """ Illustration """
+    nr_dimensions = 3
+    param_names = ["A", "B", "C"]
+    min_values = [0,0,0]
+    max_values = [1,1,1]
+    nr_values = 25 #needs to be the square of a prime number for the orthogonal case
+    S = Sampling(param_names=param_names,
+                 min_values=min_values,
+                 max_values=max_values) 
+    sample = S.latin_hypercubes(nr_values=nr_values, orthogonal=True)
+    x_values = [sample[i][0] for i in range(len(sample))]
+    y_values = [sample[i][1] for i in range(len(sample))]
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+    ax.plot(x_values, y_values, "o", markersize=10)
+    ax.set_xlabel("parameter " + param_names[0])
+    ax.set_ylabel("parameter " + param_names[1])
+    ax.set_title("Two-dimensional projection of sample points")
